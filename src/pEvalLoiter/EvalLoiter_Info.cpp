ef06b27189423e0ad92d292228fd6438c3917d8e
mikerb <mikerb@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-01-12 15:51:19 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@425 34ae1d1b-fdb2-4983-80a9-df12a7f50355

---
+++ src/pEvalLoiter/EvalLoiter_Info.cpp
@@ -0,0 +1,118 @@
+/**************************************************************/
+/*    NAME: Michael Benjamin                                  */
+/*    ORGN: Dept of Mechanical Eng / CSAIL, MIT Cambridge MA  */
+/*    FILE: EvalLoiter_Info.h                                 */
+/*    DATE: January 6th, 2016                                 */
+/**************************************************************/
+
+#include <cstdlib>
+#include <iostream>
+#include "EvalLoiter_Info.h"
+#include "ColorParse.h"
+#include "ReleaseInfo.h"
+
+using namespace std;
+
+//----------------------------------------------------------------
+// Procedure: showSynopsis
+
+void showSynopsis()
+{
+  blk("SYNOPSIS:                                                       ");
+  blk("------------------------------------                            ");
+  blk("  The pEvalLoiter application is used for evaluating vehicle    ");
+  blk("  efficiency in both (a) traversing to a loiter polygon, and    ");
+  blk("  (b) traversing the polygon once there.                        ");
+}
+
+//----------------------------------------------------------------
+// Procedure: showHelpAndExit
+
+void showHelpAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("Usage: pEvalLoiter file.moos [OPTIONS]                   ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("Options:                                                        ");
+  mag("  --alias","=<ProcessName>                                      ");
+  blk("      Launch pEvalLoiter with the given process name            ");
+  blk("      rather than pEvalLoiter.                                  ");
+  mag("  --example, -e                                                 ");
+  blk("      Display example MOOS configuration block.                 ");
+  mag("  --help, -h                                                    ");
+  blk("      Display this help message.                                ");
+  mag("  --interface, -i                                               ");
+  blk("      Display MOOS publications and subscriptions.              ");
+  mag("  --version,-v                                                  ");
+  blk("      Display the release version of pEvalLoiter.               ");
+  blk("                                                                ");
+  blk("Note: If argv[2] does not otherwise match a known option,       ");
+  blk("      then it will be interpreted as a run alias. This is       ");
+  blk("      to support pAntler launching conventions.                 ");
+  blk("                                                                ");
+  exit(0);
+}
+
+//----------------------------------------------------------------
+// Procedure: showExampleConfigAndExit
+
+void showExampleConfigAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pEvalLoiter Example MOOS Configuration                          ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  blk("ProcessConfig = pEvalLoiter                                     ");
+  blk("{                                                               ");
+  blk("  AppTick   = 4                                                 ");
+  blk("  CommsTick = 4                                                 ");
+  blk("                                                                ");
+  blk("  eval_window      = 30     // (default is 30, in seconds)      ");
+  blk("  nav_stale_thresh = 10     // (default is 10, in seconds)      ");
+  blk("  mode_thresh      = 10     // (default is 10, in meters)       ");
+  blk("  max_nav_age      = 15     // (default is 15, in seconds)      ");
+  blk("  range_max_ineff  = 50     // (default is 50, in meters)       ");
+  blk("}                                                               ");
+  blk("                                                                ");
+  exit(0);
+}
+
+
+//----------------------------------------------------------------
+// Procedure: showInterfaceAndExit
+
+void showInterfaceAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pEvalLoiter INTERFACE                                           ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("SUBSCRIPTIONS:                                                  ");
+  blk("------------------------------------                            ");
+  blk("  NODE_MESSAGE = src_node=alpha,dest_node=bravo,var_name=FOO,   ");
+  blk("                 string_val=BAR                                 ");
+  blk("                                                                ");
+  blk("PUBLICATIONS:                                                   ");
+  blk("------------------------------------                            ");
+  blk("  Publications are determined by the node message content.      ");
+  blk("                                                                ");
+  exit(0);
+}
+
+//----------------------------------------------------------------
+// Procedure: showReleaseInfoAndExit
+
+void showReleaseInfoAndExit()
+{
+  showReleaseInfo("pEvalLoiter", "gpl");
+  exit(0);
+}
+
