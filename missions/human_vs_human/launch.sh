74730f68a8dbf833f07194069efe520ad4fb34b0
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-03-10 05:53:41 +0000

    Puts the new view and coordinates in place.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@510 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/human_vs_human/launch.sh
+++ missions/human_vs_human/launch.sh
@@ -14,10 +14,10 @@ SHORE_IP="localhost"
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
 	printf "%s [SWITCHES] [time_warp]   \n" $0
-	printf "  --just_make, -j    \n" 
-	printf "  --help, -h         \n" 
+	printf "  --just_make, -j    \n"
+	printf "  --help, -h         \n"
 	exit 0;
-    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then
         TIME_WARP=$ARGI
     elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
 	JUST_MAKE="yes"
@@ -27,7 +27,7 @@ for ARGI; do
 	GOOD_GUYS="no"
     elif [ "${ARGI:0:6}" = "--amt=" ] ; then
         AMT="${ARGI#--amt=*}"
-    else 
+    else
 	printf "Bad Argument: %s \n" $ARGI
 	exit 0
     fi
@@ -55,38 +55,38 @@ if [ ! -e targ_shoreside.moos ]; then echo "no targ_shoreside.moos"; exit; fi
 
 
 #-------------------------------------------------------
-#  Part 2: Create the Mokai for human driver 1. 
+#  Part 2: Create the Mokai for human driver 1.
 #-------------------------------------------------------
-VNAME="human1"   
-START_POS="20,-20,180"         
+VNAME="human1"
+START_POS="55,25,240"
 
 nsplug meta_mokai_sim.moos targ_human_1.moos -f WARP=$TIME_WARP \
     VNAME=$VNAME           SHARE_LISTEN="9302"              \
     VPORT="9002"           SHORE_LISTEN=$SHORE_LISTEN       \
     VTEAM=$VTEAM1          START_POS=$START_POS JOY_ID="3"  \
-    BUTTON="1"             SHORE_IP=$SHORE_IP
+    BUTTON="5"             SHORE_IP=$SHORE_IP
 
 #nsplug meta_.bhv targ_mokai.bhv -f VNAME=$VNAME     \
-#    START_POS=$START_POS 
+#    START_POS=$START_POS
 
 if [ ! -e targ_human_1.moos ]; then echo "no targ_human_1.moos"; exit; fi
 #if [ ! -e targ_mokai.bhv  ]; then echo "no targ_mokai.bhv "; exit; fi
 
 
 #-------------------------------------------------------
-#  Part 3: Create the Mokai for human driver 2. 
+#  Part 3: Create the Mokai for human driver 2.
 #-------------------------------------------------------
-VNAME="human2"   
-START_POS="20,-180,0"         
+VNAME="human2"
+START_POS="-54,-108,60"
 
 nsplug meta_mokai_sim.moos targ_human_2.moos -f WARP=$TIME_WARP \
     VNAME=$VNAME           SHARE_LISTEN="9303"              \
     VPORT="9003"           SHORE_LISTEN=$SHORE_LISTEN       \
     VTEAM=$VTEAM2          START_POS=$START_POS JOY_ID="0"  \
-    BUTTON="2"             SHORE_IP=$SHORE_IP
+    BUTTON="4"             SHORE_IP=$SHORE_IP
 
 #nsplug meta_.bhv targ_mokai.bhv -f VNAME=$VNAME     \
-#    START_POS=$START_POS 
+#    START_POS=$START_POS
 
 if [ ! -e targ_human_2.moos ]; then echo "no targ_human_2.moos"; exit; fi
 #if [ ! -e targ_mokai.bhv  ]; then echo "no targ_mokai.bhv "; exit; fi
@@ -135,5 +135,5 @@ fi
 uMAC targ_shoreside.moos
 
 printf "Killing all processes ... \n"
-mykill
+kill -- -$$
 printf "Done killing processes.   \n"
