0242b65c08e86127b8b8fa38b56a0f4a2d253587
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-04-14 03:37:56 +0000

    <darpa> spring cleaning the mokai.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@620 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/darpa_demo/mokai/launch_vehicle.sh
+++ missions/darpa_demo/mokai/launch_vehicle.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-# M200_IP
-#  Emulator running on same machine as vehicle:     localhost
-#  Emulator running on different machine:           IP address of that machine (often 192.168.2.1)
-#  Actual evan vehicle:                             192.168.5.1
-#  Actual felix vehile:                             192.168.6.1
-#M200_IP="localhost"
-#M200_IP=192.168.5.1 #evan
-#M200_IP=192.168.6.1 #felix
-
-# SHORE_IP
-#  Emulation, shoreside running on same machine as vehicle: localhost
-#  Emulation, shoreside running on a different machine:     IP address of that machine (often 192.168.2.1)
-#  Actual vehicle:                                          IP address of the shoreside computer
-#SHORE_IP="localhost"
 SHORE_IP=192.168.1.155
-
+# SHORE_IP=multicast_7
 
 WARP=1
 HELP="no"
@@ -24,13 +10,10 @@ BAD_ARGS=""
 MOOS_FILE=""
 
 LOITER_POSA="x=50,y=-125"
-DOT_MOOS="meta_vehicle_fld.moos"
+DOT_MOOS="meta_mokai.moos"
 
 printf "Initiate launch vehicle script\n"
 
-#-------------------------------------------------------
-#  Part 1: Check for and handle command-line arguments
-#-------------------------------------------------------
 for ARGI; do
     UNDEFINED_ARG=$ARGI
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
@@ -44,7 +27,7 @@ for ARGI; do
         VPORT="9013"
         SHARE_LISTEN="9313"
         MOOS_FILE="targ_mokai.moos"
-        DOT_MOOS="meta_vehicle_mokai.moos"
+        DOT_MOOS="meta_mokai.moos"
         printf "MOKAI vehicle selected.\n"
         LOITER_POSV=$LOITER_POSA
     fi
@@ -58,10 +41,6 @@ for ARGI; do
     fi
 done
 
-#-------------------------------------------------------
-#  Part 2: Handle Ill-formed command-line arguments
-#-------------------------------------------------------
-
 if [ "${BAD_ARGS}" != "" ] ; then
     printf "Bad Argument: %s \n" $BAD_ARGS
     exit 0
@@ -70,20 +49,13 @@ fi
 if [ "${HELP}" = "yes" ]; then
     printf "%s [SWITCHES]            \n" $0
     printf "Switches:                \n"
-    printf "  --evan, -e             evan vehicle only                     \n"
-    printf "  --felix, -f            felix vehicle only                    \n"
     printf "  --just_build, -j       \n"
     printf "  --help, -h             \n"
     exit 0;
 fi
 
-#-------------------------------------------------------
-#  Part 3: Create the .moos and .bhv files.
-#-------------------------------------------------------
-
 printf "Assembling MOOS file ${MOOS_FILE}\n"
 
-#CRUISESPEED="1.5"
 SHORE_LISTEN="9300"
 
 nsplug ${DOT_MOOS} ${MOOS_FILE} -f  \
@@ -104,10 +76,6 @@ if [ ${JUST_BUILD} = "yes" ] ; then
     printf "Files assembled; vehicle not launched; exiting per request.\n"
     exit 0
 fi
-
-#-------------------------------------------------------
-#  Part 4: Launch the processes
-#-------------------------------------------------------
 
 printf "Launching $VNAME MOOS Community \n"
 pAntler $MOOS_FILE >& /dev/null &
