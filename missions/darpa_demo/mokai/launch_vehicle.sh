a09e0a02ab804cfa1c6472332c20312f8045c22f
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-04-14 03:37:59 +0000

    <darpa> a deeper look into mokai. todo: set groups.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@623 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/darpa_demo/mokai/launch_vehicle.sh
+++ missions/darpa_demo/mokai/launch_vehicle.sh
@@ -2,34 +2,27 @@
 
 SHORE_IP=192.168.1.155
 # SHORE_IP=multicast_7
+SHORE_LISTEN="9300"
 
 WARP=1
 HELP="no"
 JUST_BUILD="no"
 BAD_ARGS=""
-MOOS_FILE=""
 
-LOITER_POSA="x=50,y=-125"
-DOT_MOOS="meta_mokai.moos"
-
-printf "Initiate launch vehicle script\n"
+printf "Initiate launch MOKAI script.\n"
 
 for ARGI; do
     UNDEFINED_ARG=$ARGI
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-      HELP="yes"
-      UNDEFINED_ARG=""
+        HELP="yes"
+        UNDEFINED_ARG=""
     fi
     if [ "${ARGI}" = "--mokai" -o "${ARGI}" = "-m" ] ; then
-        M200_IP=192.168.1.206 #mokai
-        UNDEFINED_ARG=""
         VNAME="mokai"
         VPORT="9013"
         SHARE_LISTEN="9313"
-        MOOS_FILE="targ_mokai.moos"
-        DOT_MOOS="meta_mokai.moos"
+        UNDEFINED_ARG=""
         printf "MOKAI vehicle selected.\n"
-        LOITER_POSV=$LOITER_POSA
     fi
     if [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
         JUST_BUILD="yes"
@@ -54,21 +47,15 @@ if [ "${HELP}" = "yes" ]; then
     exit 0;
 fi
 
-printf "Assembling MOOS file ${MOOS_FILE}\n"
+printf "Assembling MOOS file targ_${VNAME}.moos .\n"
 
-SHORE_LISTEN="9300"
-
-nsplug ${DOT_MOOS} ${MOOS_FILE} -f  \
+nsplug meta_mokai.moos targ_${VNAME} -f  \
        VNAME=$VNAME                 \
        VPORT=$VPORT                 \
        WARP=$WARP                   \
        SHARE_LISTEN=$SHARE_LISTEN   \
        SHORE_LISTEN=$SHORE_LISTEN   \
        SHORE_IP=$SHORE_IP           \
-       M200_IP=$M200_IP             \
-       HOSTIP_FORCE="localhost"     \
-       LOITER_POS=$LOITER_POSV      \
-       VARIATION=$VARIATION         \
        VTYPE="kayak"                \
        VTEAM="red"
 
@@ -77,9 +64,11 @@ if [ ${JUST_BUILD} = "yes" ] ; then
     exit 0
 fi
 
-printf "Launching $VNAME MOOS Community \n"
-pAntler $MOOS_FILE >& /dev/null &
-uMAC $MOOS_FILE
+if [ ! -e targ_${VNAME}.moos ]; then echo "no targ_${VNAME}.moos!"; exit 1; fi
+
+printf "Launching $VNAME MOOS Community.\n"
+pAntler targ_${VNAME} >& /dev/null &
+uMAC targ_${VNAME}
 
 printf "Killing all processes ... \n "
 kill -- -$$
