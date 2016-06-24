ad01c3a43e93af43349e805dbac6d16e23a28e4d
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-06-24 19:08:58 +0000

    <sim> strawman script for simulation.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@758 34ae1d1b-fdb2-4983-80a9-df12a7f50355

---
+++ missions/simulated_game_1604/launch_simulation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ./m200
+./launch_m200.sh -e -b -s > /tmp/m200_blue &
+./launch_m200.sh -f -r -s > /tmp/m200_red  &
+
+cd ..
+
+cd ./mokai
+./launch_mokai.sh -b -ss > /tmp/mokai_blue &
+./launch_mokai.sh -r -ss > /tmp/mokai_red  &
+
+cd ..
+
+cd ./shoreside
+./launch_shoreside.sh
