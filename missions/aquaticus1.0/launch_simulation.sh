61e362d65a88fb24a4f87a72661c6f5138899c96
misha <misha@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-07-05 21:12:55 +0000

    adding flag structure to main launch simulation script
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@786 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/aquaticus1.0/launch_simulation.sh
+++ missions/aquaticus1.0/launch_simulation.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+ADDING_ARG=""
+UNDEFINED_ARG=""
+
+for ARGI; do
+    UNDEFINED_ARG=$ARGI
+    if [ "${ARGI}" = "-t" ] ; then
+	ADDING_ARG="-t"
+	UNDEFINED_ARG=""
+    fi
+    if [ "${UNDEFINED_ARG}" != "" ] ; then
+        BAD_ARGS=$UNDEFINED_ARG
+    fi
+done
+
+
+#-------------------------------------------------------
+#  Part 2: Handle Ill-formed command-line arguments
+#-------------------------------------------------------
+
+if [ "${BAD_ARGS}" != "" ] ; then
+    printf "Bad Argument: %s \n" $BAD_ARGS
+    exit 0
+fi
+
+
 cd ./m200
-./launch_m200.sh -e -b -s -t > /tmp/m200_blue &
+./launch_m200.sh -e -b -s $ADDING_ARG > /tmp/m200_blue &
 ./launch_m200.sh -f -r -s > /tmp/m200_red  &
 
 cd ..
