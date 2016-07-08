3f967b256476f6f46ea402a276a0786f7eb2ba34
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-07-08 16:29:05 +0000

    [aquaticus1.0] m200 supports starting position.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@793 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/aquaticus1.0/m200/launch_m200.sh
+++ missions/aquaticus1.0/m200/launch_m200.sh
@@ -8,9 +8,6 @@ TRAIL_RANGE="3"
 TRAIL_ANGLE="330"
 HELP="no"
 JUST_BUILD="no"
-EVAN="no"
-FELIX="no"
-GUS="no"
 VTEAM="red"
 
 START_POS="56,16,240"
@@ -25,73 +22,56 @@ printf "Initiate launch vehicle script\n"
 #  Part 1: Check for and handle command-line arguments
 #-------------------------------------------------------
 for ARGI; do
-    UNDEFINED_ARG=$ARGI
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
         HELP="yes"
-        UNDEFINED_ARG=""
-    fi
-    if [ "${ARGI}" = "--evan" -o "${ARGI}" = "-e" ] ; then
+    elif [ "${ARGI}" = "--evan" -o "${ARGI}" = "-e" ] ; then
         M200_IP=192.168.5.1 #evan
-        EVAN="yes"
-        UNDEFINED_ARG=""
         VNAME="evan"
         VPORT="9005"
         SHARE_LISTEN="9305"
-	TEST_TAG="YES"
         printf "EVAN vehicle selected as HUNTER.\n"
-    fi
-    if [ "${ARGI}" = "--felix" -o "${ARGI}" = "-f" ] ; then
+    elif [ "${ARGI}" = "--felix" -o "${ARGI}" = "-f" ] ; then
         M200_IP=192.168.6.1 #felix
-        FELIX="yes"
-        UNDEFINED_ARG=""
         VNAME="felix"
         VPORT="9006"
         SHARE_LISTEN="9306"
         printf "FELIX vehicle selected.\n"
-    fi
-    if [ "${ARGI}" = "--gus" -o "${ARGI}" = "-g" ] ; then
+    elif [ "${ARGI}" = "--gus" -o "${ARGI}" = "-g" ] ; then
         M200_IP=192.168.7.1 #gus
-        GUS="yes"
-        UNDEFINED_ARG=""
         VNAME="gus"
         VPORT="9007"
         SHARE_LISTEN="9307"
         printf "GUS vehicle selected as HUNTER.\n"
-    fi
-    if [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
+    elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
         JUST_BUILD="yes"
-        UNDEFINED_ARG=""
         printf "Just building files; no vehicle launch.\n"
-    fi
-    if [ "${ARGI}" = "--sim" -o "${ARGI}" = "-s" ] ; then
-        SIM="yes"
-        UNDEFINED_ARG=""
+    elif [ "${ARGI}" = "--sim" -o "${ARGI}" = "-s" ] ; then
+        SIM="SIM"
         printf "Simulation mode ON.\n"
-    fi
-    if [ "${ARGI}" = "--red" -o "${ARGI}" = "-r" ] ; then
+    elif [ "${ARGI}" = "--red" -o "${ARGI}" = "-r" ] ; then
         VTEAM="red"
-        UNDEFINED_ARG=""
         GRAB_POS="-57,-71"
         UNTAG_POS="50,-26"
         RETURN_POS="50,-26"
         START_POS="56,16,240"
         printf "Red team selected.\n"
-    fi
-    if [ "${ARGI}" = "--blue" -o "${ARGI}" = "-b" ] ; then
+    elif [ "${ARGI}" = "--blue" -o "${ARGI}" = "-b" ] ; then
         VTEAM="blue"
-        UNDEFINED_ARG=""
         GRAB_POS="50,-26"
         UNTAG_POS="-57,-71"
         RETURN_POS="-57,-71"
         START_POS="-53,-114,60"
         printf "Blue team selected.\n"
-    fi
-    if [ "${ARGI}" = "--tag" -o "${ARGI}" = "-t" ] ; then
-	START_POS="54,16,240"
-        UNDEFINED_ARG=""
-    fi
-    if [ "${UNDEFINED_ARG}" != "" ] ; then
-        BAD_ARGS=$UNDEFINED_ARG
+    elif [ "${ARGI:0:10}" = "--start-x=" ] ; then
+        START_POS_X="${ARGI#--start-x=*}"
+    elif [ "${ARGI:0:10}" = "--start-y=" ] ; then
+        START_POS_Y="${ARGI#--start-y=*}"
+    elif [ "${ARGI:0:10}" = "--start-a=" ] ; then
+        START_POS_A="${ARGI#--start-a=*}"
+    else
+        echo "Undefined argument:" $ARGI
+        echo "Please use -h for help."
+        exit 1
     fi
 done
 
@@ -99,20 +79,18 @@ done
 #  Part 2: Handle Ill-formed command-line arguments
 #-------------------------------------------------------
 
-if [ "${BAD_ARGS}" != "" ] ; then
-    printf "Bad Argument: %s \n" $BAD_ARGS
-    exit 0
-fi
-
 if [ "${HELP}" = "yes" ]; then
     printf "%s [SWITCHES]            \n" $0
     printf "Switches:                \n"
-    printf "  --evan, -e      : Evan vehicle only  \n"
-    printf "  --felix, -f     : Felix vehicle only \n"
-    printf "  --gus, -g       : Gus vehicle only   \n"
-    printf "  --blue, -b      : Blue team \n"
-    printf "  --red, -r       : Red team \n"
-    printf "  --sim, -s       : Simulation mode \n"
+    printf "  --evan, -e      : Evan vehicle only.\n"
+    printf "  --felix, -f     : Felix vehicle only.\n"
+    printf "  --gus, -g       : Gus vehicle only.\n"
+    printf "  --blue, -b      : Blue team.\n"
+    printf "  --red, -r       : Red team.\n"
+    printf "  --sim, -s       : Simulation mode.\n"
+    printf "  --start-x       : Start from x position (requires x y a).\n"
+    printf "  --start-y       : Start from y position (requires x y a).\n"
+    printf "  --start-a       : Start from angle (requires x y a).\n"
     printf "  --just_build, -j       \n"
     printf "  --help, -h             \n"
     exit 0;
@@ -122,25 +100,21 @@ fi
 #  Part 3: Create the .moos and .bhv files.
 #-------------------------------------------------------
 
+if [[ -n $START_POS_X && (-n $START_POS_Y && -n $START_POS_A)]]; then
+  START_POS="$START_POS_X,$START_POS_Y,$START_POS_A"
+  echo "Starting from " $START_POS
+elif [[ -z $START_POS_X && (-z $START_POS_Y && -z $START_POS_A) ]]; then
+  echo "Starting from default postion: " $START_POS
+else [[ -z $START_POS_X || (-z $START_POS_Y || -z $START_POS_A) ]]
+  echo "When specifing a strating coordinate, all 3 should be specified (x,y,a)."
+  echo "See help (-h)."
+  exit 1
+fi
+
 printf "Assembling MOOS file targ_${VNAME}.moos\n"
 
-if [[ -z $SIM ]]; then
-  nsplug meta_m200.moos targ_${VNAME}.moos -f \
-    VNAME=$VNAME                 \
-    VPORT=$VPORT                 \
-    WARP=$WARP                   \
-    SHARE_LISTEN=$SHARE_LISTEN   \
-    SHORE_LISTEN=$SHORE_LISTEN   \
-    SHORE_IP=$SHORE_IP           \
-    M200_IP=$M200_IP             \
-    HOSTIP_FORCE="localhost"     \
-    LOITER_POS=$LOITER_POS       \
-    VARIATION=$VARIATION         \
-    VTYPE="kayak"                \
-    VTEAM=$VTEAM                 \
-    START_POS=$START_POS
-else
-  nsplug meta_m200.moos targ_${VNAME}.moos -f \
+
+nsplug meta_m200.moos targ_${VNAME}.moos -f \
     VNAME=$VNAME                 \
     VPORT=$VPORT                 \
     WARP=$WARP                   \
@@ -154,8 +128,7 @@ else
     VTYPE="kayak"                \
     VTEAM=$VTEAM                 \
     START_POS=$START_POS         \
-    SIM
-fi
+    $SIM
 
 printf "Assembling BHV file targ_${VNAME}.bhv\n"
 nsplug meta_m200.bhv targ_${VNAME}.bhv -f  \
