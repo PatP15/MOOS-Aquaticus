9bdad27461ae443adb5f1ab8e871a4862d67033d
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-07-21 15:17:19 +0000

    [aquaticus1.0] one clean script
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@847 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/aquaticus1.0/clean.sh
+++ missions/aquaticus1.0/clean.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 VERBOSE=""
 
@@ -7,24 +7,41 @@ VERBOSE=""
 #-------------------------------------------------------
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-	printf "%s [SWITCHES]                       \n" $0
-	printf "  --verbose                         \n" 
-	printf "  --help, -h                        \n" 
-	exit 0;	
+      printf "%s [SWITCHES]                       \n" $0
+      printf "  --verbose                         \n"
+      printf "  --help, -h                        \n"
+      exit 0;
     elif [ "${ARGI}" = "--verbose" -o "${ARGI}" = "-v" ] ; then
-	VERBOSE="-v"
-    else 
-	printf "Bad Argument: %s \n" $ARGI
-	exit 0
+      VERBOSE="-v"
+    else
+      printf "Bad Argument: %s \n" $ARGI
+      exit 0
     fi
 done
 
 #-------------------------------------------------------
 #  Part 2: Do the cleaning!
 #-------------------------------------------------------
+cd m200
 
-rm -rf  $VERBOSE   MOOSLog_*  LOG_* XLOG_*
-rm -rf  $VERBOSE   */MOOSLog_*  */LOG_* */XLOG_*
+rm -rf  $VERBOSE   MOOSLog_*  LOG_*
 rm -f   $VERBOSE   *~  targ_* *.moos++
-rm -f   $VERBOSE   */*~  */targ_* */*.moos++
 rm -f   $VERBOSE   .LastOpenedMOOSLogDirectory
+
+cd ..
+
+cd mokai
+
+rm -rf  $VERBOSE   MOOSLog_*  LOG_*
+rm -f   $VERBOSE   *~  targ_* *.moos++
+rm -f   $VERBOSE   .LastOpenedMOOSLogDirectory
+
+cd ..
+
+cd shoreside
+
+rm -rf  $VERBOSE   MOOSLog_*  LOG_*
+rm -f   $VERBOSE   *~  targ_* *.moos++
+rm -f   $VERBOSE   .LastOpenedMOOSLogDirectory
+
+cd ..
