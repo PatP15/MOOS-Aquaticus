c915cfd847d2d2f1394a30b63e53c9f3f5c9a507
msis <msis@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2016-07-22 15:56:39 +0000

    [aquaticus1.0] better launch_simulation w/ time warp option too.
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@851 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/aquaticus1.0/launch_simulation.sh
+++ missions/aquaticus1.0/launch_simulation.sh
@@ -1,17 +1,70 @@
 #!/bin/bash
+TIME_WARP=4
+
+CMD_ARGS=""
+
+#-------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+        HELP="yes"
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then
+        TIME_WARP=$ARGI
+        echo "Time warp set up to $TIME_WARP."
+    elif [ "${ARGI}" = "--just_build" -o "${ARGI}" = "-j" ] ; then
+        JUST_BUILD="yes"
+        echo "Just building files; no vehicle launch."
+    else
+        CMD_ARGS=$CMD_ARGS" "$ARGI
+    fi
+done
 
 
+if [ "${HELP}" = "yes" ]; then
+  echo "$0 [SWITCHES]"
+  echo "  XX                : Time warp"
+  echo "  --just_build, -j       "
+  echo "  --help, -h             "
+  exit 0;
+fi
+
+#-------------------------------------------------------
+#  Part 2: Launching M200s
+#-------------------------------------------------------
 cd ./m200
-./launch_m200.sh -e -b -s > /tmp/m200_blue &
-./launch_m200.sh -f -r -s > /tmp/m200_red  &
-
+# Evan Blue
+./launch_m200.sh $TIME_WARP -e -b -s > /dev/null &
+# Felix Red
+./launch_m200.sh $TIME_WARP -f -r -s > /dev/null &
 cd ..
 
+#-------------------------------------------------------
+#  Part 3: Launching MOKAIs
+#-------------------------------------------------------
 cd ./mokai
-./launch_mokai.sh -e -b -ss > /tmp/mokai_blue &
-./launch_mokai.sh -f -r -ss > /tmp/mokai_red  &
-
+# Evan Blue
+./launch_mokai.sh $TIME_WARP -e -b -ss >& /dev/null &
+# Felix Red
+./launch_mokai.sh $TIME_WARP -f -r -ss >& /dev/null &
 cd ..
 
+#-------------------------------------------------------
+#  Part 4: Launching shoreside
+#-------------------------------------------------------
 cd ./shoreside
-./launch_shoreside.sh
+./launch_shoreside.sh $TIME_WARP >& /dev/null &
+cd ..
+
+#-------------------------------------------------------
+#  Part 4: Launching uMAC
+#-------------------------------------------------------
+uMAC shoreside/targ_shoreside.moos
+
+#-------------------------------------------------------
+#  Part 5: Killing all processes launched from script
+#-------------------------------------------------------
+echo "Killing Simulation..."
+kill -- -$$
+sleep 1
+echo "All processes killed"
