f7d7a597026f824a5c4fee9ce8d41132483731eb
carterf <carterf@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2018-08-14 20:47:47 +0000

    Small mod to chk_dir
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@1652 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- scripts/chk_dir.sh
+++ scripts/chk_dir.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
 
-#find -L -s $1 -type f -exec md5 {} \; > $1/md5_summary.txt
-#find -L -s $2 -type f -exec md5 {} \; > $2/md5_summary.txt
 
-
-touch $1/md5_summary.txt
+touch $1/md5_1_summary.txt
 for file in $(find -L -s $1 -type f); do
     printf "Calculating md5 for ${file}... "
 
-    printf "$(basename $file) - " >> $1/md5_summary.txt
-    md5 -q $file >> $1/md5_summary.txt
+    printf "$(basename $file) - " >> $1/md5_1_summary.txt
+    md5 -q $file >> $1/md5_1_summary.txt
 
     printf "Done!\n"
 done
 
-touch $2/md5_summary.txt
+touch $2/md5_2_summary.txt
 for file in $(find -L -s $2 -type f); do
     printf "Calculating md5 for ${file}... "
 
-    printf "$(basename $file) - " >> $2/md5_summary.txt
-    md5 -q $file >> $2/md5_summary.txt
+    printf "$(basename $file) - " >> $2/md5_2_summary.txt
+    md5 -q $file >> $2/md5_2_summary.txt
 
     printf "Done!\n"
 done
 
 
-diff_out=$(diff $1/md5_summary.txt $2/md5_summary.txt);
+diff_out=$(diff $1/md5_1_summary.txt $2/md5_2_summary.txt);
 
 if [[ "$diff_out" == "" ]]; then
     printf "Both directories are the same!\n"
@@ -36,7 +33,7 @@ else
     read r
 
     if [[ "$r" == "yes" || "$r" == "y" || "$r" == "Y" ]]; then
-        printf "$diff_out"
+        printf "$diff_out" | less
     fi
 fi
 
