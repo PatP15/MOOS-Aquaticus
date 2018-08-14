c1d38c997e5e2c62cd8e16fae07c1d31b0526e91
carterf <carterf@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2018-08-14 20:35:35 +0000

    Changed checking
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@1650 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- scripts/chk_dir.sh
+++ scripts/chk_dir.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-find -L -s $1 -type f -exec md5 {} \; > $1/md5_summary.txt
-find -L -s $2 -type f -exec md5 {} \; > $2/md5_summary.txt
 
+#find -L -s $1 -type f -exec md5 {} \; > $1/md5_summary.txt
+#find -L -s $2 -type f -exec md5 {} \; > $2/md5_summary.txt
+
+
+touch $1/md5_summary.txt
+for file in $(find -L -s $1 -type f); do
+    printf "$(basename $file) - " >> $1/md5_summary.txt
+    md5 -q $file >> $1/md5_summary.txt
+done
+
+touch $2/md5_summary.txt
+for file in $(find -L -s $2 -type f); do
+    printf "$(basename $file) - " >> $2/md5_summary.txt
+    md5 -q $file >> $2/md5_summary.txt
+done
 
 
 diff_out=$(diff $1/md5_summary.txt $2/md5_summary.txt);
