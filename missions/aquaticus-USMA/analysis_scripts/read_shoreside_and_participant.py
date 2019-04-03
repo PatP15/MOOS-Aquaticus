cc6f0120c85e1e7efe5b9dadb9963f63a1365cb7
misha <misha@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2019-04-03 08:12:02 +0000

    error checking on log file info
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@1749 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/aquaticus-USMA/analysis_scripts/read_shoreside_and_participant.py
+++ missions/aquaticus-USMA/analysis_scripts/read_shoreside_and_participant.py
@@ -1,3 +1,4 @@
+import sys
 import subprocess
 from collections import OrderedDict
 
@@ -10,6 +11,8 @@ class TrialStats:
     RED_FLAG_SCORES=0
     TIMES_TAGGED=0
     SPEECH_COMMANDED=0
+    DIALOG_ERROR=0
+    COMMAND_CANCELED=0
     TIME_FROM_AQUATICUS_GAME_START=0.0
 
 def next_ordered_key(od,key):
@@ -23,7 +26,7 @@ def next_ordered_key(od,key):
 #create a function that given the time of an event, will return the key to the appropriate scenario
 #this function will be useful for going through the participant's logs and aligning events with scenarios
 def find_proper_scenario_key_given_event_time(od,event_time):
-    keyList=sorted(d.keys())
+    keyList=sorted(od.keys())
     lastKey = next(reversed(od))
 
     #scan through the events finding the appropriate key to return
@@ -48,7 +51,7 @@ trialDictionary = OrderedDict()
 #creating a class for keeping trial information
 print "Hello, Python!"
 subprocess.call('which aloggrep',shell=True)
-subprocess.call("aloggrep -q C001_LOG_SHORESIDE_20_3_2019_____12_57_12.alog AQUATICUS_GAME* GROUP SCENARIO ROUND SELF_AUTHORIZE* RELIABLE SAY_MOOS| egrep 'AQUATICUS_GAME|GROUP|SCENARIO|ROUND|SELF_AUTHORIZE|RELIABLE|grab|goal' > shoreside_compact.txt",shell=True)
+subprocess.call("aloggrep -q C001_LOG_SHORESIDE_20_3_2019_____12_57_12.alog AQUATICUS_GAME* GROUP SCENARIO ROUND SELF_AUTHORIZE* RELIABLE SAY_MOOS| egrep 'AQUATICUS_GAME|GROUP|SCENARIO|ROUND|SELF_AUTHORIZE|RELIABLE|grab|goal|tag' > shoreside_compact.txt",shell=True)
 
 #populate important fields
 GROUP=""
@@ -72,7 +75,12 @@ for line in file:
     elif(fields[1] == "ROUND"):
         ROUND=fields[3]
     elif(fields[1] == "SAY_MOOS"):
-        if "red" in fields[3]:
+        if "deny" in fields[3]:
+            continue
+        if fields[3] == "file=tag_post_blue_one.wav":
+            latestKey = next(reversed(trialDictionary))
+            trialDictionary[latestKey].TIMES_TAGGED += 1
+        elif "red" in fields[3]:
             if "grab" in fields[3]:
                 TOTAL_FLAG_GRABS_RED = TOTAL_FLAG_GRABS_RED + 1
                 latestKey = next(reversed(trialDictionary))
@@ -93,26 +101,51 @@ for line in file:
     elif(fields[1] == "SELF_AUTHORIZE_BLUE_TWO"):
         SELF_AUTHORIZE = fields[3]
     elif(fields[1] == "SCENARIO"):
+        #using the SCENARIO as the key to the ordered dictionary
         trialDictionary[fields[3]] = TrialStats()
         latestKey = next(reversed(trialDictionary))
-        trialDictionary[latestKey].TIME_FROM_AQUATICUS_GAME_START = float(fields[0]) - SHORESIDE_GAME_START
-    elif(fields[1]=="AQUATICUS_GAME"):
+        trialDictionary[latestKey].TIME_FROM_AQUATICUS_GAME_START = float(fields[0]) - float(SHORESIDE_GAME_START)
+    elif("AQUATICUS_GAME" in fields[1]):
         if(fields[3]=="play"):
             SHORESIDE_GAME_START=fields[0]
 
+#check for if proper information in shoreside log has been found
+if GROUP=="" or ROUND=="" or SELF_AUTHORIZE=="" or SHORESIDE_GAME_START==0.0 or (len(trialDictionary)==0):
+    print("missing key game information: GROUP "+ GROUP + " ROUND: " + ROUND + " SELF_AUTHORIZE: " + SELF_AUTHORIZE + " SHORESIDE_GAME_START: " + str(SHORESIDE_GAME_START))
+    sys.exit(1)
 
 
 subprocess.call("aloggrep C001_P917_LOG_DONA_BLUE_ONE_20_3_2019_____12_57_11.alog AQUATICUS_GAME* uDialogManager uSpeechRec -q -nc -nr -gl -ac > participant_compact.txt",shell=True)
 
+PARTICIPANT_GAME_START=0.0
 file = open("participant_compact.txt","r")
 for line in file:
     fields = line.split()
     #0 is time, 1 is variable, 2 is source app, 3 is value
     print(fields[0] + " " + fields[1] + " " + fields[3])
-    if(fields[1] == "GROUP"):
-        GROUP=fields[3]
-    elif(fields[1] == "ROUND"):
-        ROUND=fields[3]
+    if("AQUATICUS_GAME" in fields[1]):
+        if(fields[3]=="play"):
+            PARTICIPANT_GAME_START=fields[0]
+    elif(fields[1]=="SPEECH_COMMANDED"):
+        #identify which key we are adding this too
+        event_time = float(fields[0]) - float(PARTICIPANT_GAME_START)
+        current_key = find_proper_scenario_key_given_event_time(trialDictionary,event_time)
+        trialDictionary[current_key].SPEECH_COMMANDED += trialDictionary[current_key].SPEECH_COMMANDED + 1
+    elif(fields[1]=="DIALOG_ERROR"):
+        #identify which key we are adding this too
+        event_time = float(fields[0]) - float(PARTICIPANT_GAME_START)
+        current_key = find_proper_scenario_key_given_event_time(trialDictionary,event_time)
+        trialDictionary[current_key].DIALOG_ERROR += trialDictionary[current_key].DIALOG_ERROR + 1
+    elif(fields[1]=="COMMAND_CANCELED"):
+        #identify which key we are adding this too
+        event_time = float(fields[0]) - float(PARTICIPANT_GAME_START)
+        current_key = find_proper_scenario_key_given_event_time(trialDictionary,event_time)
+        trialDictionary[current_key].COMMAND_CANCELED += trialDictionary[current_key].COMMAND_CANCELED + 1
+
+#check for if proper information for participant log has been found
+if PARTICIPANT_GAME_START==0.0:
+    print("Missing key participant log information: PARTICIPANT_GAME_START " + PARTICIPANT_GAME_START)
+    sys.exit(1)
 
 #test for win or loss with respect to BLUE TEAM
 if TOTAL_FLAG_SCORES_BLUE > TOTAL_FLAG_SCORES_RED:
@@ -140,6 +173,8 @@ for key, value in trialDictionary.items():
     print("Red flag scores: " + str(value.RED_FLAG_SCORES))
     print("Red flag grabs: " + str(value.RED_FLAG_GRABS))
     print("Reliable: " + value.RELIABLE)
-    print("Speech Commanded: " + str(value.SPEECH_COMMANDED))
     print("Times tagged: " + str(value.TIMES_TAGGED))
-
+    print("Speech Commanded: " + str(value.SPEECH_COMMANDED))
+    print("DIALOG_ERROR: " + str(value.DIALOG_ERROR))
+    print("COMMAND_CANCELED: " + str(value.COMMAND_CANCELED))
+ 
