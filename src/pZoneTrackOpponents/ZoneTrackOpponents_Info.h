c63f7372be999c2a741dfd0328ed3c39aeff0872
misha <misha@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2022-06-12 19:17:11 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@1856 34ae1d1b-fdb2-4983-80a9-df12a7f50355

---
+++ src/pZoneTrackOpponents/ZoneTrackOpponents_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Michael Novitzky                                     */
+/*   ORGN: MIT/USMA                                             */
+/*   FILE: ZoneTrackOpponents_Info.h                            */
+/*   DATE: June 12th, 2022                                      */
+/****************************************************************/
+
+#ifndef ZoneTrackOpponents_INFO_HEADER
+#define ZoneTrackOpponents_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
