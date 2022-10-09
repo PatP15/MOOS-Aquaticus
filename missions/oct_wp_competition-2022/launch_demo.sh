7f0fb72b9eb64eca196715bd0d572b580c109225
misha <misha@34ae1d1b-fdb2-4983-80a9-df12a7f50355>
2022-10-09 00:18:46 +0000

    updated field points for wp oct 2022 event
    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-aquaticus-oai/trunk@1874 34ae1d1b-fdb2-4983-80a9-df12a7f50355

--- missions/oct_wp_competition-2022/launch_demo.sh
+++ missions/oct_wp_competition-2022/launch_demo.sh
@@ -51,16 +51,16 @@ echo Logging to $LOGDIR
 if [[ -z $NO_HERON ]]; then
   cd ./surveyor
   # Gus Red
-  ./launch_surveyor.sh s r1 r2 $TIME_WARP $LOGDIR -s --start-x=250 --start-y=160 --start-a=240 --role=ATTACK_E > /dev/null &
+  ./launch_surveyor.sh s r1 r2 $TIME_WARP $LOGDIR -s --start-x=250.23 --start-y=156.03 --start-a=201.3 --role=ATTACK_E > /dev/null &
   sleep 1
   # Luke Red
-  ./launch_surveyor.sh t r2 r1 $TIME_WARP $LOGDIR -s --start-x=262 --start-y=156 --start-a=240 --role=DEFEND_E > /dev/null &
+  ./launch_surveyor.sh t r2 r1 $TIME_WARP $LOGDIR -s --start-x=259.55 --start-y=152.39 --start-a=201.3 --role=DEFEND_E > /dev/null &
   sleep 1
   # Kirk Blue
-  ./launch_surveyor.sh u b1 b2 $TIME_WARP $LOGDIR -s --start-x=232 --start-y=75 --start-a=60 --role=ATTACK_MED > /dev/null &
+  ./launch_surveyor.sh u b1 b2 $TIME_WARP $LOGDIR -s --start-x=221.17 --start-y=81.49 --start-a=21.3 --role=ATTACK_MED > /dev/null &
   sleep 1
   # Jing Blue
-  ./launch_surveyor.sh v b2 b1 $TIME_WARP $LOGDIR -s --start-x=220 --start-y=80 --start-a=60 --role=DEFEND_MED > /dev/null &
+  ./launch_surveyor.sh v b2 b1 $TIME_WARP $LOGDIR -s --start-x=230.49 --start-y=77.85 --start-a=21.3 --role=DEFEND_MED > /dev/null &
   sleep 1
   cd ..
 fi
